e8e65e4a5c04d0ee0b21323bd569f08430845d51
Oleg <oleg-okatev@bk.ru>
2021-02-24 19:50:47 +0300

    [Server] Connect database

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const mongoose = require('mongoose');
 const app = require('./server');
-const { port, ip } = require('./config.json');
+const { port, ip, databaseURL } = require('./config.json');
 
-app.listen(port, ip, () => console.log(`Server is running on ${ip}:${port}`));
+mongoose.connect(databaseURL, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+    if (err) throw err;
+    app.listen(port, ip, () => console.log(`Server is running on ${ip}:${port}`));
+});
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
-const { port, ip } = require('./config.json');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.raw())
+app.use(bodyParser.json())
+app.use(bodyParser.text())
 
 app.get('/', (req, res) => {
     res.send(`<h1>Hello World!</h1>`);
+});
+
+
+// register route
+app.use('/api', require('./routes/auth'));
+
+
+// error handling
+app.use((err, req, res, next) => {
+    if (err.error) {
+        res.status(401).json(err);
+    }
+    next();
 });
 
 module.exports = app;
